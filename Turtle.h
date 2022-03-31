fa0d43e57e9b9f0da14a3553e544a9024e34fd50
DanyaKrov <74681279+DanyaKrov@users.noreply.github.com>
2022-03-31 10:31:40 +0300

    Чтение файла и проверка валидности значений

---
+++ Turtle.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <Windows.h>
+using namespace std;
+
+
+
+int str_to_int(string str);
+void check(vector <string> map, int xpos, int ypos);
---
+++ main.cpp
@@ -0,0 +1,35 @@
+﻿#include "Turtle.h"
+
+void read_file(vector <string>& map, int& xpos, int& ypos, string path)
+{
+	ifstream file;
+	string str;
+	int number = 0;
+	file.open(path);
+	if (file.is_open()) {
+		while (getline(file, str)) {
+			number = str_to_int(str);
+			if (number < -60000) {
+				map.push_back(str);
+			}
+			else if (xpos == -101)
+				xpos = number;
+			else if (ypos == -101)
+				ypos = number;
+		}
+	}
+	else {
+		cout <<"Error: Operation file corrupted" <<endl;
+		exit(0);
+	}
+	file.close();
+}
+
+int main()
+{
+	int xpos = -101, ypos = -101;
+	string mode = "fast";
+	vector<string> map;
+	read_file(map, xpos, ypos, "input.txt");
+	check(map, xpos, ypos);
+}
